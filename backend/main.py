023df0e1422f91af63a00649f3ece1ab073e06e1
eleven <accounts_1053463733794149@mail.aliyun.com>
2024-06-15 20:24:09 +0800

    add async

--- backend/main.py
+++ backend/main.py
@@ -8,17 +8,21 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class StockInfo(BaseModel):
     code: str
     name: str
+
 
 @app.get("/")
 async def read_root():
     return FileResponse("html/index.html")
 
+
 @app.get("/page/stock_show")
 async def read_stock_show():
     return FileResponse("html/stock_show.html")
+
 
 @app.get("/page/stock_list")
 async def read_stock_list():
@@ -26,12 +30,12 @@ async def read_stock_list():
 
 
 @app.get("/api/stocks")
-def get_stocks():
+async def get_stocks():
     return g_stock_info_list
 
 
 @app.post("/api/stocks")
-def add_stock(stock: StockInfo):
+async def add_stock(stock: StockInfo):
     for existing_stock in g_stock_info_list:
         if existing_stock["code"] == stock.code:
             continue
@@ -40,7 +44,7 @@ def add_stock(stock: StockInfo):
 
 
 @app.delete("/api/stocks/{stock_code}")
-def delete_stock(stock_code: str):
+async def delete_stock(stock_code: str):
     for stock in g_stock_info_list:
         if stock["code"] == stock_code:
             g_stock_info_list.remove(stock)
@@ -50,7 +54,7 @@ def delete_stock(stock_code: str):
 
 # http://localhost:8000/api/stock/data/603628
 @app.get("/api/stock/data/{stock_code}")
-def stock_data_day(stock_code: str):
+async def stock_data_day(stock_code: str):
     data = get_stock_data_list(stock_code)
     data_dict = data.to_dict(orient="records")
     return {"b_code": 200, "data": data_dict}
@@ -58,11 +62,13 @@ def stock_data_day(stock_code: str):
 
 # http://localhost:8000/api/stock/echarts/line/603628
 @app.get("/api/stock/echarts/line/{stock_code}")
-def stock_data_day_echart(stock_code: str, last_day_count: int = 14):
+async def stock_data_day_echart(stock_code: str, last_day_count: int = 14):
     data = get_stock_data_list(stock_code, last_day_count)
     data_dict = convert_to_echarts_multi_line_format(data)
     return JSONResponse(content={"b_code": 200, "data": data_dict})
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=PORT)
