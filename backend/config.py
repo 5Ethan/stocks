089c32b0a8df4bd508859f0a5731859ddffc6935
eleven <accounts_1053463733794149@mail.aliyun.com>
2024-06-15 18:30:33 +0800

    update script

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 
 PORT = 8888
 
-g_data_dir = os.path.dirname(__file__)
+g_data_dir = os.path.expanduser("~") if os.name == 'posix' else os.path.dirname(__file__)
 
 g_stock_info_list = [
     {"code": "603628", "name": "清源股份"},
