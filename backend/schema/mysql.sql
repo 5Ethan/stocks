e342af71efa2bf4c116bb8fc20e90cca2302e501
eleven <accounts_1053463733794149@mail.aliyun.com>
2024-06-24 19:34:05 +0800

    update schema

--- backend/schema/mysql.sql
+++ backend/schema/mysql.sql
@@ -3,3 +3,11 @@ CREATE TABLE info_stock (
     stock_code VARCHAR(20) NOT NULL COMMENT '股票编码',
     stock_name VARCHAR(100) NOT NULL COMMENT '股票名称'
 )  COMMENT '股票信息表';
+
+CREATE TABLE info_dict_item (
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    dict_key VARCHAR(32) NOT NULL COMMENT '字典key',
+    dict_item VARCHAR(32) NOT NULL COMMENT '字典项',
+    dict_value VARCHAR(128) NOT NULL COMMENT '字典值',
+    dict_comment VARCHAR(128) NOT NULL COMMENT '字典项备注'
+)  COMMENT '字典信息表';
